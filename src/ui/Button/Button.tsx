e9e33cada3dd7844b644a574af3eb5935a94f384
Neksten <denchik7366@gmail.com>
2023-07-31 21:47:07 +0400

    Added ui component button

---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React, { ButtonHTMLAttributes, FC } from 'react';
+import './Button.scss';
+
+type ButtonProps = {
+  bgColor: string;
+  small?: boolean;
+  medium?: boolean;
+  big?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+// prettier-ignore
+const Button: FC<ButtonProps> = ({
+	                                 bgColor,
+	                                 children,
+	                                 small,
+	                                 medium,
+	                                 big,
+	                                 ...props
+                                 }) => {
+	
+	const rootClasses: string[] = ['ui-button'];
+	if (small) {
+		rootClasses.push('small-ui-btn');
+	}
+	if (medium) {
+		rootClasses.push('medium-ui-btn');
+	}
+	if (big) {
+		rootClasses.push('big-ui-btn');
+	}
+	
+	return (
+		<button {...props} style={{backgroundColor: bgColor}} className={rootClasses.join(' ')}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
